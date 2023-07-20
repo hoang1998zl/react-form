13bbb7e09f6eb95818be91858d779a76f203d97d
hoang1998zl <hhoaang@gmail.com>
2023-07-20 21:18:25 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import Form from "./components/Form";
-import ListUsers from "./components/ListUsers";
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import ListUsers from './components/ListUsers';
+import Action from './components/Action';
 
 function App() {
   return (
-    <div className="App container my-5">
-      <h1 className='w-100 p-4 bg-dark text-white fw-bold'>
-        Thông tin sinh viên
-      </h1>
-      <Form />
-      <h1 className='w-100 p-4 bg-dark text-white fw-bold'>
-        Danh sách sinh viên
-      </h1>
-      <ListUsers />
-    </div>
+    <Provider store={store}>
+      <div className="App container my-5">
+        <h1 className='w-100 p-4 bg-dark text-white fw-bold'>
+          Thông tin sinh viên
+        </h1>
+        <ListUsers />
+        <Action />
+      </div>
+    </Provider>
   );
 }
 
